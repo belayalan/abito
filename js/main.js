4f1e00b299a3a29eafcf47f5db014b83ad361b61
Akmaral <toreyevaazz@gmail.com>
2021-07-28 23:38:33 +0500

    2page

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+        loop: false,
+        spaceBetween: 10,
+        slidesPerView: 6,
+        freeMode: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+      });
+      var swiper2 = new Swiper(".mySwiper2", {
+        loop: true,
+        spaceBetween: 10,
+  
+        thumbs: {
+          swiper: swiper,
+        },
+      });
