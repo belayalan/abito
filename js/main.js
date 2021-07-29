1fdcfeb3da1f0ea65a734cebbf70de25864ba62e
Akmaral <toreyvaazz@gmail.com>
2021-07-29 19:06:01 +0500

    3page

--- js/main.js
+++ js/main.js
@@ -14,3 +14,15 @@ var swiper = new Swiper(".mySwiper", {
           swiper: swiper,
         },
       });
+
+      const menuBtn = document.querySelector('.menu-button');
+      const menu = document.querySelector('.navbar-panel');
+      const closeMenu = document.querySelector('.close-menu');
+
+      menuBtn.addEventListener('click', () => {
+        menu.classList.toggle('is-open');
+      });
+
+      closeMenu.addEventListener('click', () => {
+        menu.classList.toggle('is-open');
+      });
